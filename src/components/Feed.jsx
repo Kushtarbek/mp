e26e11d9d0db7a55d835ecd9a75c402cc6b0c0f1
Kushtarbek <dr.kushtarbek@gmail.com>
2020-11-21 18:29:48 -0600

    added small components

---
+++ src/components/Feed.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Feed = () => {
+    return (
+        <div className="content">
+            {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlU8V9gP8YGrPOMrRQgjXH03iqu2j1OTzSqw&usqp=CAU"></img> */}
+            <div>
+                <img alt="" src="https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg"></img>
+            </div>
+            <div>ava+ desc</div>
+            <div>my posts
+                <div>post 1</div>
+                <div>post 2</div>
+             </div>
+
+        </div>
+    )
+}
+
+export default Feed;
